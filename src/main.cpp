553a7928aaa8d964ff957309f08618bacd0621bc
crack-haddock <richardmoss@outlook.com>
2024-01-24 11:32:12 +0000

    test 9

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
   
-  std::cout << "Game has terminated successfully!\n";
+  std::cout << "Game has terminated successfully!!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
   
